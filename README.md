c7cd7f040fe29806c49e95c889ec5dc8b84de7ba
Eddy Luten <eddyluten@gmail.com>
2015-02-07 21:11:30 -0800

    Added Brew Cask support and some more defaults.

--- README.md
+++ README.md
@@ -1,9 +1,30 @@
 # mac-setup
 
-A bash script for OS X which preps your Mac for development.
+A bash script for OS X which preps your Mac for development. Optionally installs
+my .bash_profile file from
+[this GitHub repo](https://github.com/EddyLuten/bash_profile).
 
 ## Installation
 
 Download `setup.sh` and execute the file or copy and paste this one-liner into your bash terminal:
 
     bash -i <(curl -s https://raw.githubusercontent.com/EddyLuten/mac-setup/master/setup.sh)
+
+## What it Does
+
+This script sets a couple of sane Finder defaults and installs the latest stable versions of:
+
+* Xcode Command Line Tools (optional)
+* Homebrew
+* Homebrew Cask
+* wget
+* Vim
+* Git
+* Node.js
+* rbenv
+* A default .bash_profile
+* A Ruby version through a prompt (optional)
+* Common applications (optional)
+    * Atom
+    * Google Chrome
+    * Mozilla Firefox
--- setup.sh
+++ setup.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
+BASH_PROFILE_URL="https://raw.githubusercontent.com/EddyLuten/bash_profile/master/.bash_profile"
+
+command_failed()
+{
+  [ $? -ne 0 ]
+}
+command_succeeded()
+{
+  [ $? -eq 0 ]
+}
 
 ask_user()
 {
   echo "$1 (y/n)"
   read USER_RESULT
 
-  echo "$USER_RESULT" | grep -i "y"
-  if [ $? -eq 0 ]; then
+  echo "$USER_RESULT" | grep -i "y" &> /dev/null
+  if command_succeeded; then
     return 0
   else
     return 1
@@ -14,21 +24,24 @@ ask_user()
 }
 
 # OS X Settings
-defaults write com.apple.finder AppleShowAllFiles YES
+defaults write com.apple.finder AppleShowAllFiles -bool YES
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool YES
+defaults write com.apple.finder ShowPathBar -bool YES
+defaults write com.apple.finder AppleShowAllExtensions -bool YES
 killall Finder
 
 # Prompt to install xcode command line tools
-if ask_user "Do you wish to install xcode command line tools?"; then
-  echo 'Installing xcode command line tools...'
+if ask_user "Do you wish to install Xcode command line tools?"; then
+  echo 'Installing Xcode command line tools...'
   xcode-select --install # Will prompt
-  echo 'IMPORTANT: please let the xcode installation finish before continuing.'
+  echo 'IMPORTANT: please let the Xcode installation finish before continuing.'
 else
-  echo 'Not installing xcode command line tools.'
+  echo 'Not installing Xcode command line tools.'
 fi
 
 # Install required tools
 brew --version &> /dev/null
-if [ ! $? -eq 0 ]; then
+if command_failed; then
   echo 'Installing homebrew...'
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   brew doctor
@@ -37,19 +50,33 @@ else
   brew update
 fi
 
+# See what's installed already and store it in a variable
+BREW_LIST=$(brew list)
+
+# Install brew cask for installing applications
+echo "$BREW_LIST" | grep brew-cask &> /dev/null
+if command_failed; then
+  brew install caskroom/cask/brew-cask
+fi
+
 # Install wget
 wget -V -nv &> /dev/null
-if [ ! $? -eq 0 ]; then
+if command_failed; then
   echo 'Installing wget...'
   brew install wget
-else
-  echo 'Upgrading wget...'
-  brew upgrade wget
+fi
+
+# Install Vim
+echo "$BREW_LIST" | grep vim &> /dev/null
+if command_failed; then
+  echo 'Installing Vim...'
+  brew install vim
+  brew install macvim --override-system-vim
 fi
 
 # Install git
-brew list | grep git
-if [ ! $? -eq 0 ]; then
+echo "$BREW_LIST"  | grep git &> /dev/null
+if command_failed; then
   echo 'Installing git...'
   brew install git
 
@@ -64,35 +91,26 @@ if [ ! $? -eq 0 ]; then
     git config --global user.name "$GIT_USER"
     git config --global user.email "$GIT_EMAIL"
   fi
-else
-  echo 'Upgrading git...'
-  brew upgrade git
 fi
 
 # Install node.js
 node --version &> /dev/null
-if [ ! $? -eq 0 ]; then
+if command_failed; then
   echo 'Installing node.js...'
   brew install node
-else
-  echo 'Upgrading node.js...'
-  brew upgrade node
 fi
 
 # Install rbenv
 rbenv --version &> /dev/null
-if [ ! $? -eq 0 ]; then
+if command_failed; then
   echo 'Installing rbenv...'
   brew install rbenv ruby-build
-else
-  echo 'Upgrading rbenv...'
-  brew upgrade rbenv ruby-build
 fi
 
 # Ensure that a .bash_profile exists
 if ask_user "Do you wish to install a default .bash_profile?"; then
   echo 'Downloading a default .bash_profile...'
-  wget -q -O ~/.bash_profile http://bit.ly/1zEPNup
+  wget -q -O ~/.bash_profile "$BASH_PROFILE_URL"
   chmod +x ~/.bash_profile
   source ~/.bash_profile
 else
@@ -102,7 +120,7 @@ fi
 
 # Add the rbenv initialization to .bash_profile
 grep 'rbenv init' ~/.bash_profile &> /dev/null
-if [ ! $? -eq 0 ]; then
+if command_failed; then
   echo 'Adding rbenv init to .bash_profile...'
   echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
   source ~/.bash_profile
@@ -114,14 +132,42 @@ if ask_user "Do you wish to install a specific Ruby version now?"; then
   echo 'Please enter the version you wish to install:'
   read USER_RUBY_VERSION
   echo "Installing Ruby $USER_RUBY_VERSION..."
-  rbenv install $USER_RUBY_VERSION
+  rbenv install "$USER_RUBY_VERSION"
 
-  if [ $? -eq 0 ]; then
+  if command_succeeded; then
     if ask_user 'Do you wish to make this the default (global) Ruby version?'; then
-      rbenv global $USER_RUBY_VERSION
+      rbenv global "$USER_RUBY_VERSION"
     fi
   else
     echo 'Ruby installation failed.'
+  fi
+fi
+
+if ask_user "Do you wish to install commonly used applications now?"; then
+  CASK_LIST=$(brew cask list)
+  APPS_LIST=$(ls /Applications/)
+  BOTH_LISTS="$CASK_LIST\nAPPS_LIST"
+
+  # Install Atom
+  printf "$BOTH_LISTS" | egrep -i "atom(.app)?" &> /dev/null
+  if command_failed; then
+    echo 'Installing Atom...'
+    brew cask install atom
+    brew cask install atom-shell
+  fi
+
+  # Install Google Chrome
+  echo "$BOTH_LISTS" | egrep -i "google[ -]chrome(.app)?" &> /dev/null
+  if command_failed; then
+    echo 'Installing Google Chrome...'
+    brew cask install google-chrome
+  fi
+
+  # Install Firefox
+  echo "$BOTH_LISTS" | egrep -i "firefox(.app)?" &> /dev/null
+  if command_failed; then
+    echo 'Installing Firefox...'
+    brew cask install firefox
   fi
 fi
 
